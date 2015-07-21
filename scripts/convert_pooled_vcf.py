cf1f8059f2dff4e08f228d2adf782bcda0472966
Vikas Bansal <vikas115@gmail.com>
2015-07-20 17:30:29 -0700

    added file to convert CRISP allele count output to standard polyploid VCF

---
+++ scripts/convert_pooled_vcf.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+import sys, os, glob, string, subprocess,time, math, re, compiler
+
+## coded july 15 2015, code to convert CRISP pooled allele count to genotype 0/0/0/0/0/1 format so that it is compatible with standard tools
+
+if len(sys.argv) < 3: 
+	print >>sys.stderr, "\n\nPROGRAM to convert CRISP VCF for pooled samples to a standard VCF where genotypes are in the form 0/0/0/0/0/1"; 
+	print >>sys.stderr, "\nHOW TO RUN: python convert_pooled_VCF.py VCF_file_from_CRISP.vcf bam_file_list poolsize(optional) > NEW_VCF_file.vcf "; 
+	print >>sys.stderr, "\nNOTE: poolsize should be specified as the third argument when all pools have the same pool size\n\n";
+	sys.exit();
+if len(sys.argv) > 3: 
+	poolsize = int(sys.argv[3]); 
+else: poolsize = -1; 
+	
+bam_file_list = sys.argv[2]; 
+File = open(bam_file_list);
+pool_sizes = []; 
+for line in File: 
+	pool = line.strip().split();
+	if len(pool) >=2 and 'PS=' in pool[1]: pool_sizes.append(int(pool[1].split('=')[1])); 
+	elif poolsize < 0: print >>sys.stderr, "pool size information is not available, it should be provided on the command line (same pool size for all pools) or using the bam_file_list file"; sys.exit(); 
+File.close(); 
+print >>sys.stderr, "pool sizes:", pool_sizes; #sys.exit();
+
+variants=0; 
+File = open(sys.argv[1]); # pooled VCF file
+ 
+for line in File:
+
+	if line[0] == '#': 
+		print line,
+		continue;
+
+	var = line.strip().split('\t'); chrom = var[0]; position = int(var[1]); alleles = var[4].split(',');
+
+	for i in xrange(8): sys.stdout.write(var[i] + "\t")
+	#sys.stdout.write(var[8]);
+	col8 = var[8].split(':')[1:]; new_col8 = 'GT:' + ':'.join(col8); 
+	sys.stdout.write(new_col8); # replace AlleleCount by Genotype 
+
+	for i in xrange(9,len(var)):
+		genotype = var[i].split(':'); counts = genotype[0].split(','); 
+
+		if poolsize > 0: 
+			refsum = poolsize; GV = [];
+
+			if genotype[0] == '.': 
+				for c in xrange(poolsize): GV.append('.');
+			else: 
+				for c in xrange(len(counts)): refsum -= int(counts[c]); 
+				if refsum < 0: print >>sys.stderr, "poolsize is smaller than allele counts, please provide correct pool size, terminating program"; sys.exit();
+				for c in xrange(refsum): GV.append('0'); 
+				for a in xrange(len(counts)): 
+					for c in xrange(int(counts[a])):  GV.append(`(a+1)`); 
+			#print 'genotype','/'.join(GV);					
+		else: ## variable pool size so genotype is different 
+			GV = [];
+			if genotype[0] == '.': 
+				for c in xrange(pool_sizes[i-9]): GV.append('.');
+				#print >>sys.stderr,'ERROR',genotype
+				#pass;
+				## what should we do, need input bam file list 
+			else: 
+				for a in xrange(len(counts)): 
+					for c in xrange(int(counts[a])):  GV.append(`(a)`); 
+				if len(GV) > pool_sizes[i-9]: print >>sys.stderr, "poolsize is smaller than allele counts, please provide correct pool size, terminating program"; sys.exit();
+			
+		sys.stdout.write('\t' + '/'.join(GV) + ':' +  ':'.join(genotype[1:]));
+	
+	sys.stdout.write('\n')
+
+File.close();
