9e8a6907c39ffade0b5583df77d80e2b41fcc647
Vikas Bansal <vikas115@gmail.com>
2015-07-24 13:57:17 -0700

    updates to code for converting CRISP output VCF to standard VCF for polyploid organisms

--- scripts/convert_pooled_vcf.py
+++ scripts/convert_pooled_vcf.py
@@ -3,6 +3,8 @@ import sys, os, glob, string, subprocess,time, math, re, compiler
 
 ## coded july 15 2015, code to convert CRISP pooled allele count to genotype 0/0/0/0/0/1 format so that it is compatible with standard tools
 
+## if the sum of allele counts is more than poolsize, program ignores that variant. 
+
 if len(sys.argv) < 3: 
 	print >>sys.stderr, "\n\nPROGRAM to convert CRISP VCF for pooled samples to a standard VCF where genotypes are in the form 0/0/0/0/0/1"; 
 	print >>sys.stderr, "\nHOW TO RUN: python convert_pooled_VCF.py VCF_file_from_CRISP.vcf bam_file_list poolsize(optional) > NEW_VCF_file.vcf "; 
@@ -33,11 +35,8 @@ for line in File:
 
 	var = line.strip().split('\t'); chrom = var[0]; position = int(var[1]); alleles = var[4].split(',');
 
-	for i in xrange(8): sys.stdout.write(var[i] + "\t")
-	#sys.stdout.write(var[8]);
-	col8 = var[8].split(':')[1:]; new_col8 = 'GT:' + ':'.join(col8); 
-	sys.stdout.write(new_col8); # replace AlleleCount by Genotype 
-
+	valid_variant = 1;
+	GENOTYPES = []; 
 	for i in xrange(9,len(var)):
 		genotype = var[i].split(':'); counts = genotype[0].split(','); 
 
@@ -48,7 +47,11 @@ for line in File:
 				for c in xrange(poolsize): GV.append('.');
 			else: 
 				for c in xrange(len(counts)): refsum -= int(counts[c]); 
-				if refsum < 0: print >>sys.stderr, "poolsize is smaller than allele counts, please provide correct pool size, terminating program"; sys.exit();
+				if refsum < 0: 
+					print >>sys.stderr, "poolsize is smaller than allele counts, please provide correct pool size, terminating program";
+					print >>sys.stderr, refsum,var[i],var[0:5]
+					valid_variant = 0; break; 
+					sys.exit();
 				for c in xrange(refsum): GV.append('0'); 
 				for a in xrange(len(counts)): 
 					for c in xrange(int(counts[a])):  GV.append(`(a+1)`); 
@@ -63,10 +66,26 @@ for line in File:
 			else: 
 				for a in xrange(len(counts)): 
 					for c in xrange(int(counts[a])):  GV.append(`(a)`); 
-				if len(GV) > pool_sizes[i-9]: print >>sys.stderr, "poolsize is smaller than allele counts, please provide correct pool size, terminating program"; sys.exit();
+				if len(GV) > pool_sizes[i-9]: 
+					valid_variant = 0; break; 
+					print >>sys.stderr, "poolsize is smaller than allele counts, please provide correct pool size, terminating program"; 
+					sys.exit();
 			
-		sys.stdout.write('\t' + '/'.join(GV) + ':' +  ':'.join(genotype[1:]));
+		#sys.stdout.write('\t' + '/'.join(GV) + ':' +  ':'.join(genotype[1:]));
+		GENOTYPES.append('/'.join(GV) + ':' +  ':'.join(genotype[1:]));
 	
-	sys.stdout.write('\n')
+	if valid_variant ==0: print >>sys.stderr, "filtering variant due to alleles",var[0:5]; continue;
+	col8 = var[8].split(':')[1:]; new_col8 = 'GT:' + ':'.join(col8); newVec = var[0:8]; newVec.append(new_col8); 
+
+	print "\t".join(newVec + GENOTYPES).strip() 
+	#sys.stdout.write(var[8]);
+	#print >>sys.stderr, newVec + GENOTYPES
+	#print "\t".join(GENOTYPES).strip() 
+	#sys.stdout.write('\n')
 
 File.close();
+
+
+# python convert_pooled_vcf.py /media/drive2/T2D-pooledseq-july2012/phase3-78pools/phase1.CRISP.vcf.annotated /media/drive2/T2D-pooledseq-july2012/phase1-190pools/plist.190pools 40 > 190pools.phase1.genotypes.vcf
+
+# python convert_pooled_vcf.py /media/drive2/T2D-pooledseq-july2012/phase3-78pools/VCFs/plist.68pools.VCF.080714 /media/drive2/T2D-pooledseq-july2012/phase3-78pools/VCFs/plist.68pools > 68pools.new.vcf
